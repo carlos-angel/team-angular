015e4118f900c3f911433035b8a5777d123fb885
carlos-angel <c.a.a.a.503@gmail.com>
2022-01-01 17:48:46 -0600

    feat: eliminar un jugador

--- src/app/player-table/player-table.component.ts
+++ src/app/player-table/player-table.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Player } from '../interfaces/player.interface';
 import { PlayerService } from '../services/player.service';
+import { TeamService } from '../services/team.service';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-player-table',
@@ -13,7 +15,7 @@ export class PlayerTableComponent implements OnInit {
   public selectedPlayer: Player;
   public showModal: boolean = false;
 
-  constructor(private playerService: PlayerService) {
+  constructor(private playerService: PlayerService, private teamService: TeamService) {
     this.players$ = this.playerService.getPlayers();
   }
 
@@ -33,6 +35,21 @@ export class PlayerTableComponent implements OnInit {
     setTimeout(() => {
       window.location.replace('#open-modal');
     }, 0);
+  }
+
+  deletePlayer(player: Player) {
+    this.teamService
+      .getTeams()
+      .pipe(take(1))
+      .subscribe((teams) => {
+        const modifiedPlayers = teams[0].players ? teams[0].players.filter((p: any) => p.key !== player.$key) : teams[0].players;
+        const formattedTeam = {
+          ...teams[0],
+          players: [...modifiedPlayers],
+        };
+        this.playerService.deletePlayer(player.$key);
+        this.teamService.editTeam(formattedTeam);
+      });
   }
 
   closeDialog() {
